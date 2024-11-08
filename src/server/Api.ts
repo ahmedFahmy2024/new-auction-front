e082800eac7b3de2b837e5d8c785df0b1e27d7a5
Ahmed Fahmy <ahmedmewans2030@gmail.com>
2024-11-08 19:25:38 +0200

    finishing-1

--- src/server/Api.ts
+++ src/server/Api.ts
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://auctionbackend-von8.onrender.com/api/v1";
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://auctionbackend-von8.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:8000/api/v1";
 const PROJECTS = "/projects";
 const AUCTIONS = "/auctions";
 const PRICES = "/prices";
