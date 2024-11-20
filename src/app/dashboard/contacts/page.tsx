bdd269b1c8214b8d75592808653aedfbfbe0f140
Ahmed Fahmy <ahmedmewans2030@gmail.com>
2024-11-20 22:58:11 +0200

    10-58-11-20

--- src/app/dashboard/contacts/page.tsx
+++ src/app/dashboard/contacts/page.tsx
@@ -1,16 +1,20 @@
 import ContactTable from "@/components/dashboard/contact/ContactTable";
+import Loading from "@/components/Loading";
+import { Suspense } from "react";
 
-const page = () => {
+const ContactPage = async () => {
   return (
     <main className="min-h-screen p-8 mt-[80px]">
       <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
         <h2 className="text-2xl font-bold">التواصل</h2>
       </div>
       <div className="mt-8">
-        <ContactTable />
+        <Suspense fallback={<Loading />}>
+          <ContactTable />
+        </Suspense>
       </div>
     </main>
   );
 };
 
-export default page;
+export default ContactPage;
